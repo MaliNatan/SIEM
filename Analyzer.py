9d15a610877efe7d5cb6ec420511fedec1219ac7
MaliNatan <mali28080@gmail.com>
2018-05-23 13:41:20 +0300

    Copying changes

--- Analyzer.py
+++ Analyzer.py
@@ -30,22 +30,9 @@ def PortScan():
             dic[ip]+=1
         else:
             dic[ip]=1
-    #print dic
     for x in dic.iteritems():
         if x[1]>10:
             print "Find Port Scan attack from ", ''.join(x[0]), "to", x[1], "ports"
-    '''dic={}
-    for c in cursor:
-        ip=c[0],'-',c[1]
-        port=c[2]
-        if ip not in dic.keys():
-            dic[ip] = {}
-        dic[ip].add(port)
-
-        #dic['IP']=c[0]+'-'+c[1]
-        #dic['PORT']=c[2]
-    print dic'''
-
     cnx.commit()
     cursor.close()
     cnx.close()
